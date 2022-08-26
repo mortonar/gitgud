d021ee4364cd8cb7d4ef326e0cd73e5b12d24c8e
Alex Morton <mortonar01@gmail.com>
2022-08-26 13:14:24 -0400

    Basic small commit list with rudimentary navigation

--- src/git.rs
+++ src/git.rs
@@ -1,58 +1,79 @@
 use crate::cli::Cli;
+use crossterm::event::{Event, KeyCode};
+use crossterm::style::Stylize;
+use crossterm::{terminal, ExecutableCommand};
 use git2::{Commit, Error, Repository};
+use std::io::stdout;
 
-pub fn open_repo(cli: Cli) -> Result<(), Error> {
+/// Abstraction wrapper over git2::Repository
+pub struct Repo {
+    repository: Repository,
+}
+
+pub fn open_repo(cli: Cli) -> Result<Repo, Error> {
     let repo = Repository::open(&cli.repo)?;
-
-    let state = repo.state();
-    println!("{}({:?}):", &cli.repo, &state);
-    println!();
-
-    let head = get_head_commit(&repo)?;
-    let mut stack = Vec::new();
-    stack.push(head);
-    for _i in 0..=10 {
-        if let Some(c) = stack.pop() {
-            display_commit_info(&c);
-            stack.push(c.parent(0)?);
-        }
-    }
-    // let mut revwalk = match repo.revwalk() {
-    //     Ok(revwalk) => revwalk,
-    //     Err(e) => panic!("failed to get revwalk: {}", e)
-    // };
-    // revwalk.set_sorting(Sort::NONE).expect("set revwalk sorting failed");
-    // revwalk.push_head().expect("revwalk push HEAD failed");
-    // if let Some(Ok(oid)) = revwalk.next() {
-    //     let commit = match repo.find_commit(oid) {
-    //         Ok(commit) => commit,
-    //         Err(e) => panic!("couldn't find commit: {}", e)
-    //     };
-    //     display_commit_info(&commit);
-    // }
-    Ok(())
+    Ok(Repo { repository: repo })
 }
 
 pub fn get_head_commit(repo: &Repository) -> Result<Commit, Error> {
     repo.head()?.peel_to_commit()
 }
 
-pub fn display_commit_info(commit: &Commit) {
-    println!("-----------------------------------------------------------------------------------");
-    println!("Author: {}", commit.author());
-    println!("{}", commit.message().expect("no commit!"));
+pub fn get_commit_info(commit: &Commit) -> String {
+    format!(
+        "Author: {}| {}",
+        commit.author(),
+        commit.summary().expect("no commit!")
+    )
+}
 
-    // if let Ok(tree) = commit.tree() {
-    //     for tree_entry in tree.iter() {
-    //         if let Some(ot) = tree_entry.kind() {
-    //             if ot == ObjectType::Blob {
-    //                 println!("{}:", tree_entry.name().unwrap());
-    //                 let object = tree_entry.borrow().to_object(&repo).unwrap();
-    //                 let content = String::from_utf8_lossy(object.as_blob().unwrap().content());
-    //                 println!("blob: {}", &content);
-    //             }
-    //         }
-    //     }
-    // }
-    println!("-----------------------------------------------------------------------------------");
+pub struct CommitList {
+    selected: usize,
+    commits: Vec<GGCommit>,
+}
+
+impl CommitList {
+    pub fn display(&self) {
+        stdout()
+            .execute(terminal::Clear(terminal::ClearType::All))
+            .unwrap();
+        for (i, c) in self.commits.iter().enumerate() {
+            if i == self.selected {
+                println!("{}\r", &c.info.clone().on_blue().on_blue());
+            } else {
+                println!("{}\r", &c.info);
+            }
+        }
+    }
+
+    pub fn event(&mut self, event: Event) {
+        if event == Event::Key(KeyCode::Char('j').into()) {
+            self.selected = (self.selected + 1) % self.commits.len();
+        } else if event == Event::Key(KeyCode::Char('k').into()) {
+            self.selected = self.selected.saturating_sub(1);
+        }
+    }
+}
+
+struct GGCommit {
+    info: String,
+}
+
+pub fn get_commit_list(repo: &Repo) -> Result<CommitList, Error> {
+    let head = get_head_commit(&repo.repository)?;
+    let mut stack = Vec::new();
+    let mut commit_list = Vec::new();
+    stack.push(head);
+    for _i in 0..=10 {
+        if let Some(c) = stack.pop() {
+            commit_list.push(GGCommit {
+                info: get_commit_info(&c),
+            });
+            stack.push(c.parent(0)?);
+        }
+    }
+    Ok(CommitList {
+        selected: 0,
+        commits: commit_list,
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,26 @@
 use crossterm::event::{read, Event, KeyCode};
-use crossterm::style::Stylize;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use crossterm::{terminal, ExecutableCommand};
 use gitgud::cli;
 use gitgud::git;
+use gitgud::git::get_commit_list;
 use io::Result;
 use std::io;
-use std::io::stdout;
-
-struct Line {
-    text: &'static str,
-}
 
 fn main() -> Result<()> {
-    // let cli = cli::Cli::default();
-    // git::open_repo(cli).unwrap();
+    let cli = cli::Cli::default();
+    let repo = git::open_repo(cli).unwrap();
 
-    let mut lines = Vec::new();
-    lines.push(Line {
-        text: "This is the first line",
-    });
-    lines.push(Line {
-        text: "This is the second line",
-    });
-    let mut selected = 0;
     enable_raw_mode()?;
+    let mut commit_list = get_commit_list(&repo).unwrap();
     loop {
-        draw_lines(&lines, selected);
-        // It's guaranteed that read() won't block if `poll` returns `Ok(true)`
+        commit_list.display();
         let event = read()?;
-        if event == Event::Key(KeyCode::Char('j').into()) {
-            selected = (selected + 1) % lines.len();
-        } else if event == Event::Key(KeyCode::Char('k').into()) {
-            selected = selected.saturating_sub(1);
-        } else if event == Event::Key(KeyCode::Char('q').into()) {
+        if event == Event::Key(KeyCode::Char('q').into()) {
             break;
+        } else {
+            commit_list.event(event);
         }
     }
     disable_raw_mode()?;
     Ok(())
-}
-
-fn draw_lines(lines: &Vec<Line>, selected: usize) {
-    stdout()
-        .execute(terminal::Clear(terminal::ClearType::All))
-        .unwrap();
-    print!("selected: {}", &selected);
-    for (i, l) in lines.iter().enumerate() {
-        if i == selected {
-            println!("{}\r", &l.text.on_blue().on_blue());
-        } else {
-            println!("{}\r", &l.text);
-        }
-    }
 }
